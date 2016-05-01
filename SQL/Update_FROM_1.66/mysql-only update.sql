3444656905b10186d924a6e0cc9f61bb102471f4
Rui Mendes <Rui Mendes>
2016-05-01 18:31:13 +0100

    Default Package

---
+++ SQL/Update_FROM_1.66/mysql-only update.sql
@@ -0,0 +1,10 @@
+-- ---------------------------------------------------------------------------
+--
+--					ebookcms Update mySQL (FROM 1.66) - All rights reserved
+--					Copyright (c) 2015 by Rui Mendes
+--					Revision: 0
+--
+-- --------------------------------------------------------------------------
+
+-- UPDATE core_version
+UPDATE `config` SET 'field' = 'core_version', 'value' = 'ebookcms_1.68' WHERE id = 1;
